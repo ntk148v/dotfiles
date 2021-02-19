1ce1c765ad34d28d44c7f5978c85e3b06b702a6c
Kien Nguyen <kiennt65@viettel.com.vn>
2021-02-19 09:14:13 +0700

    Scripted auto-commit on change (2021-02-19 09:14:13) by gitwatch.sh

--- home/.scripts/powermenu-compact.sh
+++ home/.scripts/powermenu-compact.sh
@@ -1,13 +1,34 @@
-chosen=$(echo -e "Cancel\nLogout\nShutdown\nReboot\nSuspend\nHibernate" | rofi -dmenu -i -l 6 -fullscreen 1)
 
-if [[ $chosen = "Logout" ]]; then
-	i3-msg exit
-elif [[ $chosen = "Shutdown" ]]; then
-	systemctl poweroff
-elif [[ $chosen = "Reboot" ]]; then
-	systemctl reboot
-elif [[ $chosen = "Suspend" ]]; then
-	systemctl suspend
-elif [[ $chosen = "Hibernate" ]]; then
-	systemctl hibernate
-fi
+#!/bin/sh
+
+# options to be displayed
+option0="⏻   Shutdown"
+option1="􀅈   Reboot"
+option2="􀎠   Lock"
+option3="⎋   Logout"
+option4="􀟙   Suspend"
+
+uptime=$(uptime -p | sed -e 's/up //g')
+
+# options passed into variable
+options="$option0\n$option1\n$option2\n$option3\n$option4"
+
+chosen="$(echo -e "$options" | rofi -theme ~/.config/rofi/powermenu.rasi -lines 5 -dmenu -p "$uptime")"
+
+case $chosen in
+    $option0)
+        loginctl poweroff
+        ;;
+    $option1)
+        loginctl reboot
+        ;;
+    $option2)
+        betterlockscreen --lock blur
+        ;;
+	$option3)
+        killall -u $USER
+        ;;
+    $option4)
+        loginctl suspend
+        ;;
+esac
